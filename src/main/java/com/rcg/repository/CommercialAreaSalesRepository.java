41170d35141fee49c85f3d814e1beb7f89727eab
Federico-15 <peter1256@skuniv.ac.kr>
2025-04-27 11:00:09 +0900

    🐛:디렉토리 구조 수정

---
+++ src/main/java/com/rcg/repository/CommercialAreaSalesRepository.java
@@ -0,0 +1,7 @@
+package com.rcg.repository;
+
+import com.rcg.entity.CommercialAreaSales;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommercialAreaSalesRepository extends JpaRepository<CommercialAreaSales, String> {
+}
