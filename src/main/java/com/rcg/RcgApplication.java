3fdde1e15ce29d5e79f0ba0471b8b8e4e36b181c
chaeelin <chaeee0123@gmail.com>
2025-05-02 04:35:28 +0900

    :sparkles:Feat: 스케줄링 기능 활성화를 위한 @EnableScheduling 추가

--- src/main/java/com/rcg/RcgApplication.java
+++ src/main/java/com/rcg/RcgApplication.java
@@ -2,8 +2,10 @@ package com.rcg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RcgApplication {
 
 	public static void main(String[] args) {
